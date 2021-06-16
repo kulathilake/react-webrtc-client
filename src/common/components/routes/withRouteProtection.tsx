819bc1fd88e9c30c0761109d7b34efc7459c6a1b
Shehan Kulathilake <shehanhere@gmail.com>
2021-06-17 00:47:00 +0530

    router protection wrapper

--- src/common/components/routes/withRouteProtection.tsx
+++ src/common/components/routes/withRouteProtection.tsx
@@ -5,7 +5,15 @@ import { Route as RouteProps } from "../../types/route.types";
 
 export default function withRouteProtection(Component: React.ComponentType<any>,redirect: string): React.FC<any> {
     return function(props: RouteProps) {
-        const {isAuthenticated, user} = useAuth();
+        const {isAuthenticated} = useAuth();
+        /**
+         * Checks if the user is authorized (isAuthenticated being true is a given)
+         * as per the permission requirement and user permissions, to view the route.
+         * WARNING: Client Side route protection is not enough to actually protect the
+         * data behind these routes. Always use server sider authorization when handling 
+         * requests.
+         * @returns boolean
+         */
         const isAuthorized = (): boolean=> {
             if(!isAuthenticated) return false;
             else return true;
