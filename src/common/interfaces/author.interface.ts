cbbb180940c31af5d94310dd36e987fe25d15773
Shehan Kulathilake <shehanhere@gmail.com>
2021-06-16 17:31:30 +0530

    restructured folders

--- src/common/interfaces/author.interface.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "./user.interface";
-
-export interface Author {
-    id: string;
-    user: User;
-    privateKey: string;
-    publicKey: string;
-    
-}
--- src/common/interfaces/crypto.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Crypto {
-    getCandidateKeys(): Promise<string>;
-    generateSecret(): Promise<string>;
-    signString(stringToSign: string): Promise<string>;
-    encrypt(string: string, key: string): Promise<string>;
-    decrypt(cipher: string, key: string): Promise<string>;
-}
