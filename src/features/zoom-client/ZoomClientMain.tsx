413e9499b559ff9a9d62f5f7df5589149029605b
Shehan Kulathilake <shehanhere@gmail.com>
2021-07-16 13:49:53 +0530

    Zoom Client from Generic Feature

---
+++ src/features/zoom-client/ZoomClientMain.tsx
@@ -0,0 +1,8 @@
+import { FeatureMainViewProps } from "./types";
+import withInjectedProps from "./components/ZoomClientWrapper";
+
+export function FeatureMainView(){
+    return null;
+};
+
+export default withInjectedProps<FeatureMainViewProps>(FeatureMainView);
---
+++ src/features/zoom-client/zoomClientSlice.ts
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initState = {
+
+};
+
+const featureSlice = createSlice({
+    name: 'featureSlice',
+    initialState: initState,
+    reducers: {
+
+    }
+});
+
+export default featureSlice.reducer;
